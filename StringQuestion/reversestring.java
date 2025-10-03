8bd955b3df895d2bcef77aa8ca8e878d4fd57055
akshitm <akshitm970@gmail.com>
2025-10-03 12:22:26 +0530

    update

--- StringQuestion/reversestring.java
+++ StringQuestion/reversestring.java
@@ -3,9 +3,9 @@ package StringQuestion;
 public class reversestring {
     public static void main(String[] args) {
         String s = "Infinite";
-        String k = "";
+        StringBuilder k = new StringBuilder();
         for(int i = s.length()-1;i>0;i--){
-            k += s.charAt(i);
+            k.append(s.charAt(i));
         }
         System.out.println(k);
     }
